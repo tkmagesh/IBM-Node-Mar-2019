2c2941a815262379139a6da557219a488ff44631
Magesh <tkmagesh77@gmail.com>
2019-03-18 17:23:48 +0530

    Day-01 Complete

--- 00-assignment/untitled.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var server = http.createServer();
-
-server.listen(8080);
-
-server.on('listening', function(){
-	console.log('server listening on 8080!');
-});
--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,13 +1,17 @@
 var http = require('http'),
 	fs = require('fs'),
-	path = require('path');
+	path = require('path'),
+	url = require('url')
 
 var server = http.createServer(function(req /* IncomingMessage */, res /*ServerResponse */){
 	
 	console.log(req.url);
 
-	var resourceName = req.url === '/' ? 'index.html' : req.url,
+	var resource = req.url === '/' ? 'index.html' : req.url,
+		urlObj = url.parse(resource),
+	 	resourceName = urlObj.pathname,
 		resourceFullName = path.join(__dirname, resourceName);
+		
 	if (!fs.existsSync(resourceFullName)){
 		res.statusCode = 404;
 		res.end();
