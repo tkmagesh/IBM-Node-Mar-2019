59d94a7d0ef4f5c1d16e2ee11644fb2d686d0099
Magesh <tkmagesh77@gmail.com>
2019-03-19 11:07:54 +0530

    web app server handling post requests

--- 07-web-app-server/server.js
+++ 07-web-app-server/server.js
@@ -30,7 +30,7 @@ var server = http.createServer(function(req, res){
 		}
 		var stream = fs.createReadStream(resourceFullName);
 		stream.pipe(res);
-	} else if ( resourceName === '/calculator'){
+	} else if ( resourceName === '/calculator' && req.method === 'GET'){
 		var queryData = querystring.parse(urlObj.query);
 		var op = queryData.op,
 			x = parseInt(queryData.x),
@@ -39,6 +39,21 @@ var server = http.createServer(function(req, res){
 
 		res.write(result.toString());
 		res.end();
+	} else if ( resourceName === '/calculator' && req.method === 'POST'){
+		var rawData = '';
+		req.on('data', function(chunk){
+			rawData += chunk;
+		});
+		req.on('end', function(){
+			var bodyData = querystring.parse(rawData);
+			var op = bodyData.op,
+				x = parseInt(bodyData.x),
+				y = parseInt(bodyData.y),
+				result = calculator[op](x,y);
+
+			res.write(result.toString());
+			res.end();	
+		});
 	} else {
 		res.statusCode = 404;
 		res.end();
