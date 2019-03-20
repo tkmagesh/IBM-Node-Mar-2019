a15bcf2bf012df03ab8cadcf1859f40bae3beef8
Magesh <tkmagesh77@gmail.com>
2019-03-20 12:04:26 +0530

    updates and removal of tasks

--- 10-task-manager/routes/taskApiRoutes.js
+++ 10-task-manager/routes/taskApiRoutes.js
@@ -22,6 +22,37 @@ router.post('/', function(req, res,){
 		};
 		taskList.push(newTask);
 		res.status(201).json(newTask);
-})
+});
+
+router.put('/:id', function(req, res){
+	var taskIdToUpdate = parseInt(req.params.id),
+		taskDataToUpate = req.body;
+	var taskToReplace = taskList.find(function(task){
+		return task.id === taskIdToUpdate;
+	});
+	if (!taskToReplace){
+		res.status(404).end();
+		return;
+	}
+	taskList = taskList.map(function(task){
+		return task.id === taskIdToUpdate ? taskDataToUpate : task;
+	});
+	res.status(200).json(taskDataToUpate);
+});
+
+router.delete('/:id', function(req, res){
+	var taskIdToDelete = parseInt(req.params.id);
+	var taskToRemove = taskList.find(function(task){
+		return task.id === taskIdToDelete;
+	});
+	if (!taskToRemove){
+		res.status(404).end();
+		return;
+	}
+	taskList = taskList.filter(function(task){
+		return task.id !== taskIdToDelete;
+	});
+	res.status(200).json({});
+});
 
 module.exports = router;
